fbc9ed9c82e288c51c1bb9f3326c3c0b0da840b7
zvrkhanuma <almazalovic@gmail.com>
2021-03-17 17:51:09 +0100

    Added second object index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+var myClothes = {
+    hat: "Fedora",
+    shirt: "Nike",
+    shoes: "Converse"
+}
+function myFunction(myObj){
+    var hatValue = myObj.hat;
+    var shirtValue = myObj.shirt;
+    var shoesValue = myObj.shoes;
+    return { hatValue, shirtValue, shoesValue };
+}
+// Only change code above this line
+console.log(myFunction(myClothes));
+module.exports = myFunction(myClothes);
